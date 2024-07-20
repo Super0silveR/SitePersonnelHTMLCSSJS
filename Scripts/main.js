33c01a0c9cdedb5c5427dcb665d0f42459484bd8
BearWulF <elias.sidi@gmail.com>
2024-07-19 22:44:29 -0400

    Mobile responsible

--- Scripts/main.js
+++ Scripts/main.js
@@ -14,3 +14,25 @@ workCardElement.forEach((workCard) => {
     workCard.classList.add("active");
   });
 });
+
+function burgerMenu() {
+  var menu = document.getElementById("optionmenu");
+  if (menu.style.display === "grid") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "grid";
+  }
+}
+
+function closeOptionMenu() {
+  var menu = document.getElementById("optionmenu");
+  menu.style.display = "none";
+}
+
+function openWorkCard(id) {
+  var card = document.getElementById(id);
+}
+
+function closeWorkCard(id) {
+  var card = document.getElementById(id);
+}
